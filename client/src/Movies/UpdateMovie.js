6f7e860b94e7acd357d3d6e32836fdc6f0d8f783
Eian Carter <eian@bricleir.com>
2020-01-30 09:21:47 -0800

    building out edit form

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, { useState, useParams } from 'react';
+import axios from 'axios';
 
 
 const UpdateMovie = (props) => {
+    const initialMovie = {
+        title: '',
+        director: '',
+        metascore: 0,
+        stars: []
+    }
+    const [ editingMovie, setEditingMovie ] = useState(initialMovie)
+    const id = useParams()
+
+    useEffect(() => {
+        const movieToUpdate = props.movies.find(thing => `${thing.id}` === id);
+
+        if (movieToUpdate) {
+          setItem(movieToUpdate);
+        }
+    },[props.movies, id])
+    
+    const SubmitNewMovie = e => {
+        e.preventDefault();
+        axios
+            .put("/api/movies/:id")
+            .then(res => {
+                console.log(res)
+
+            })
+            .catch(err => console.log(err))
+    }
+    const changeHandler = e => {
+        e.preventDefault();
+        setEditingMovie()
+    }
+
     return (
         <div>
-            <form onSubmit={}>
+            <form onSubmit={SubmitNewMovie}>
                 <input
                     type='text'
                     name='title'
                     placeholder='title'
-                    onChange={}
+                    onChange={changeHandler}
                 />
                 <input
                     type='text'
                     name='director'
                     placeholder='director'
-                    onChange={}
+                    onChange={changeHandler}
                 />
                 <input
                     type='text'
                     name='metascore'
                     placeholder='metascore'
-                    onChange={}
+                    onChange={changeHandler}
                 />
                 <input
                     type='text'
                     name='stars'
                     placeholder='stars'
-                    onChange={}
+                    onChange={changeHandler}
                 />
             </form>
         </div>
